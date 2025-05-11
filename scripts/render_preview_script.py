6f8883321fbdc11f0e1b15cf2f5dc30726bdd944
nekoshaurman <mrrft2@gmail.com>
2025-05-11 18:17:17 +0300

    Work with blender.exe, that not in PATH

--- scripts/render_preview_script.py
+++ scripts/render_preview_script.py
@@ -3,7 +3,9 @@ import sys
 import bpy
 import traceback
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+# sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_root)
 
 import util.utils as utils
 
